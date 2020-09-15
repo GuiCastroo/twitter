2f2e2b6770d8d5391c8959005545a62377392dba
guilhermeinstacarro <guilherme.castro@instacarro.com>
2020-09-15 19:21:24 -0300

    Build config for get access

---
+++ config.py
@@ -0,0 +1,19 @@
+"""
+Module for config credential of twitter
+"""
+
+import twitter
+
+
+def token(token, token_secret, consumer_key, consumer_secret):
+    """
+     Functions for create access for Twitter API.
+
+    :param token:
+    :param token_secret:
+    :param consumer_key:
+    :param consumer_secret:
+    :return:
+
+    """
+    return Twitter(auth=OAuth(token, token_secret, consumer_key, consumer_secret))
